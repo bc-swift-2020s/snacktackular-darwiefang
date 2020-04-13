18eca3dc19408b385ee0192731d8afb5f64a0dde
Darwie Fang <fangdb@bc.edu>
2020-04-13 22:25:17 +1000

    9.2

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by darwie fang on 13/4/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
